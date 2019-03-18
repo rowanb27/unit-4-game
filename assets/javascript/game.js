d4b3df01511bce0a93afd986ebb5a1c83f357b57
Robert Rowan <rowanb27@yahoo.com>
2019-03-18 13:15:38 -0500

    updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,4 +29,26 @@ $(document).ready(function() {
             enamyAttackBack: 25
         }
     };
+      console.log(characters);
+
+      var renderOne = function(character, renderArea) {
+          var charDiv = $("<div class='character' data-name'" + character.name + "'>");
+          var charName = $("<div class='character-name'>").text(character.name);
+          var charImage = $("<img atl='image' class='character-image'>").attr("src", character.imageUrl);
+          charDiv.append(charName).append(charImage).append(charHealth);
+          $(renderArea).append(charDiv);
+      }
+
+      var renderCharacters = function(charObj, areaRender) {
+          if(areaRender === "#characters-section") {
+              $(areaRender).empty();
+              for (var key in charObj) {
+                  if(charObj.hasOwnProperty(key)) {
+                      renderOne(charObj[key], areaRender);
+                  }
+              }
+          }
+      }
+
+     
 });
