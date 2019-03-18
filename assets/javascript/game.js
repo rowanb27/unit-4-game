b3d979b71f66957ba9b6f6b86d9a8cbc17e0aa58
Robert Rowan <rowanb27@yahoo.com>
2019-03-18 10:42:41 -0500

    added photos

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    var characters = {
+        "Dr. Mario": {
+            name: "Dr. Mario",
+            health: 120,
+            attack: 8,
+            imageUrl: "assets\images\Dr. Mario.jpg",
+            enamyAttackBack: 15
+        },
+        "Peach": {
+            name: "Peach",
+            health: 100,
+            attack: 14,
+            imageUrl: "assets\images\Peach.jpg",
+            enamyAttackBack: 5
+        },
+        "Marth": {
+            name: "Marth",
+            health: 150,
+            attack: 8,
+            imageUrl: "assets\images\Marth.jpg",
+            enamyAttackBack: 5
+        },
+        "Falco": {
+            name: "Falco",
+            health: 180,
+            attack: 7,
+            imageUrl: "assets\images\Falco.jpg",
+            enamyAttackBack: 25
+        }
+    };
+});
